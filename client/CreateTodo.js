8f80331faec24c0137c7309068a1ca20698b967c
Chaplaso-Ola <chaplaso@yahoo.com>
2019-03-12 20:49:48 -0400

    first commit

--- client/CreateTodo.js
+++ client/CreateTodo.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react'
 
 export default class CreateTodo extends Component {
+  constructor(){
+    super()
+    this.state = {
+      taskName: '',
+      assignee: '',
+    }
+  }
+  handleChange = ({target}) => {
+    this.setState({[target.name]:target.value})
+  }
   render () {
     return (
-      <form></form>
+      <form>
+        <label htmlFor="taskName"> taskName: </label>
+        <input name="taskName" type="text" value="taskName"/>
+
+        <label htmlFor="assignee" onChange={handleChange}> assignee: </label>
+        <input name="assignee" type="text" value="assignee"/>
+
+        <button type="submit"> Submit</button>
+
+      </form>
     )
   }
 }
