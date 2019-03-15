59b56c678c081c8b35b56482ee33ef835c0f5d2d
Juan Torres <jet@Juans-MacBook-Pro.local>
2019-03-14 20:25:41 -0400

    updated todoform

--- client/CreateTodo.js
+++ client/CreateTodo.js
@@ -3,9 +3,30 @@ import axios from 'axios'
 import TodoForm from './TodoForm'
 
 export default class CreateTodo extends Component {
+  constructor(){
+    super()
+    this.state = {
+      taskName: '',
+      assignee: '',
+    }
+  }
+  handleChange = ({target}) => {
+    // console.log(target)
+    this.setState({[target.name]: target.value})
+    }
+
+  handleSubmit = (e) => {
+  e.preventDefault();
+  // console.log('this is the state', this.state);
+  axios.post('/api/todos', {...this.state})
+      .then((response) => response.data)
+      .then((data) => this.props.addTodo(data))
+      .catch(err => console.log(err));
+  }
+
   render () {
     return (
-      <TodoForm addTodo={this.props.addTodo} />
+      <TodoForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
     )
   }
 }
